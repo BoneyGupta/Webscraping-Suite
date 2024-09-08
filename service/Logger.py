b2f46dcb762a2b80bc15f218e099c878f654f9cf
aaryaman_gupta <aaryamangupta1997@gmail.com>
2024-09-08 23:14:49 +0530

    test

--- service/Logger.py
+++ service/Logger.py
@@ -6,7 +6,24 @@ from utils.Logging import Logging
 
 class Logs:
     def __init__(self):
+
         # Create a runtime directory for reports
+        directory = f"Report"
+        try:
+            os.mkdir(directory)
+            print("Directory created successfully.")
+        except FileExistsError:
+            print("Directory already exists.")
+
+        # Create a runtime directory for chromedata
+        directory = f"chromedata"
+        try:
+            os.mkdir(directory)
+            print("Directory created successfully.")
+        except FileExistsError:
+            print("Directory already exists.")
+
+            # Create a runtime directory for reports in Reports
         directory = f"Report {time.strftime('%Y-%m-%d %H%M%S')}"
         self.directory_path = f"Reports/{directory}"
         try:
@@ -20,7 +37,6 @@ class Logs:
         # self.data = Logging(f"{self.directory_path}/data.log", False)
         # self.ref_data = Logging(f"{self.directory_path}/reference data.log", False)
         # self.ref_loop_data = Logging(f"{self.directory_path}/reference loop data.log", False)
-
 
         self.code_prog.info("Logger initialized")
         self.log.info("Logger initialized")
